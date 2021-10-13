9cfb768d5159a88db98e108442dcf65ff877f541
AmulyaGulati <amulyagulati@gmail.com>
2021-10-13 17:55:48 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDYddupMWra5vmun6udJvtrH8k1FkgifA8',
+  authDomain: 'chat-web-app-ebca8.firebaseapp.com',
+  projectId: 'chat-web-app-ebca8',
+  storageBucket: 'chat-web-app-ebca8.appspot.com',
+  messagingSenderId: '978358535847',
+  appId: '1:978358535847:web:29e6c0873726ad613e7d82',
+};
+
+const app = firebase.initializeApp(config);
