66e5d41f2ba39f489cfb8e4412c060ed7a13b785
AmulyaGulati <amulyagulati@gmail.com>
2021-10-14 19:28:35 +0530

    Added Sign In Page and Sign In with Facebook/Google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyDYddupMWra5vmun6udJvtrH8k1FkgifA8',
@@ -11,3 +13,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
