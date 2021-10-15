c24be59f67aba6b94451bcf3c6846c999bf5fd33
AmulyaGulati <amulyagulati@gmail.com>
2021-10-15 16:33:30 +0530

    Started building Sidebar Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Grid>
+      <Grid fluid className="h-100">
+        <Row>
+          <Col xs={24} md={8}>
+            <Sidebar />
+          </Col>
+        </Row>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Home;
