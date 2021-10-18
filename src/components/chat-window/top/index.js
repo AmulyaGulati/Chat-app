1d08c14750c3e880906dca06ecea1969ab6ee81a
AmulyaGulati <amulyagulati@gmail.com>
2021-10-18 18:03:58 +0530

    Created Chat Bottom, Send Messages to the Chat

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -12,7 +12,7 @@ const Top = () => {
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h4>
+        <h4 className="text-disappear d-flex align-items-center">
           <Icon
             componentClass={Link}
             to="/"
@@ -26,13 +26,12 @@ const Top = () => {
           />
           <span className="text-disappear">{name}</span>
         </h4>
-
         <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
 
-        <div className="d-flex justify-content-between align-items-center">
-          <span>todo</span>
-          <RoomInfoBtnModal />
-        </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
       </div>
     </div>
   );
