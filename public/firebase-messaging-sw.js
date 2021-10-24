926b6464e8904e63a91c939e0c73d23e5f2328b4
AmulyaGulati <amulyagulati@gmail.com>
2021-10-24 15:42:45 +0530

    Added service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts(
-  'https://www.gstatic.com/firebasejs/7.15.0/firebase-messaging.js'
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js'
 );
 
 const firebaseApp = initializeApp({
